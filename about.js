48768c0c47a571559b6d968d1fbdd9db321189f3
Bryton Haldey <bryton.hadley@gmail.com>
2022-09-02 15:51:15 -0600

    uptdate to the picuter alert

--- about.js
+++ about.js
@@ -1,23 +1,34 @@
 console.log("hello world");
 
+//setp one: selecting an element
+let form = document.querySelector('#contact');
+
+//step two: write a function
 function handleSubmit(evt) {
 	evt.preventDefault();
 	
-	alert('form submit successfully');
+	alert('It submit successfully!');
 }
 
 
 
-let form = document.querySelector('#contact');
-
+// step 3: combine them with eventlistener
 form.addEventListener('submit', handleSubmit);
 
+// mouseover event focusing on image
 
 let mouse = document.querySelector('#img-mouse')
 
+// const mouseOverCompliment = () => {
+// 	alert(' Lokk it is a Duck!')
+// }
+
+// mouse.addEventListener('mouseover', mouseOverCompliment)
+
+// write a function to alert with a message
 const mouseOver = (event) => {
 	event.preventDefault();
-
+	alert('It is a Duck!')
 	mouse.style.opacity = 0.5;
 
 }
@@ -27,6 +38,7 @@ const mouseOut = (event) => {
 
 	mouse.style.opacity = 1.0;
 }
+
 
 mouse.addEventListener('mouseover', mouseOver)
 
